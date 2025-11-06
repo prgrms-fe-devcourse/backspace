f0d7a58e51ce8c80ad7d88527bd524080f3857ef
stupilman <creworroad@naver.com>
2025-11-06 17:27:43 +0900

    feat(button): 버튼 컴포넌트 구현

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { button, type ButtonVariantProps } from "./variants";
+
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button">, ButtonVariantProps {}
+
+/**
+ * 공통 버튼 컴포넌트
+ *
+ * 버튼 스타일, 사이즈, 형태 등을 조합할 수 있도록 CVA를 활용해 구현했습니다.
+ *
+ * @component
+ * @param {"lg" | "md" | "sm"} [size="sm"] - 버튼 크기
+ * @param {"iconOnly" | "textOnly" | "iconText" } [composition="iconText"] - 아이콘/텍스트 조합 형태
+ * @param {"neutral" | "pressed" | "focus" | "inactive"} [state="neutral"] - 버튼 스타일 유형
+ * @param {"full" | "auto"} [width="auto"] - 버튼 폭 정의
+ * @param {string} [className] - 추가 Tailwind 클래스
+ * @param {React.ReactNode} children - 버튼 내부 콘텐츠
+ * @param {React.MouseEventHandler<HTMLButtonElement>} [onClick] - 클릭 이벤트 핸들러
+ * @returns {JSX.Element} 버튼 엘리먼트
+ *
+ * @example
+ * ```tsx
+ * <Button intent="primary" size="md" onClick={() => alert("clicked")}>
+ *   <Plus /> 저장하기
+ * </Button>
+ * ```
+ */
+
+export default function Button({
+  size = "sm",
+  composition = "iconText",
+  state: controlledState,
+  width = "auto",
+  disabled,
+  className,
+  children,
+  onClick,
+  ...rest
+}: ButtonProps) {
+  const [state, setState] = useState<"neutral" | "pressed" | "focus" | "inactive">(
+    controlledState ?? "neutral"
+  );
+
+  return (
+    <button
+      type="button"
+      onMouseDown={() => setState("pressed")}
+      onMouseUp={() => setState("focus")}
+      onBlur={() => setState("neutral")}
+      onClick={onClick}
+      disabled={disabled}
+      className={twMerge(
+        button({
+          size,
+          state,
+          composition,
+          width,
+        }),
+        className
+      )}
+      {...rest}
+    >
+      <span
+        className={twMerge(
+          "flex items-center justify-center",
+          composition === "iconText" && size === "sm" && "gap-1",
+          composition === "iconText" && size === "md" && "gap-1.5",
+          composition === "iconText" && size === "lg" && "gap-2",
+          disabled && "opacity-40"
+        )}
+      >
+        {children}
+      </span>
+
+      {state === "focus" && (
+        <div className={twMerge("pointer-events-none absolute inset-1 border border-dashed")} />
+      )}
+    </button>
+  );
+}
