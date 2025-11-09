4ae1159a3742e516fcb35434973bb6a6f16a90db
stupilman <creworroad@naver.com>
2025-11-09 17:49:56 +0900

    refactor(titlebar): variants.ts에서 accent-gradient를 사용하도록 수정, 크기에서 px, py 사이즈 분리

--- src/components/TitleBar/variants.ts
+++ src/components/TitleBar/variants.ts
@@ -2,11 +2,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 export const titleBar = cva(
   // Base styles:
-  [
-    "flex w-full cursor-default items-center select-none",
-    // TODO: 아래 gradient 색상은 이후 global.css에서 가져와 수정할 예정
-    "w-full bg-linear-to-r from-[#B2AAEB] to-[#AAC1F8] px-1 py-1.5",
-  ],
+  ["flex w-full cursor-default items-center select-none", "accent-gradient w-full"],
   {
     variants: {
       // 상태 (Active, Inactive)
@@ -16,8 +12,8 @@ export const titleBar = cva(
       },
       // 크기 (Small, Medium)
       size: {
-        small: "h-6 text-xs leading-3",
-        medium: "h-8 text-sm leading-3.5",
+        small: "h-6 px-[5px] py-[3px] text-xs leading-3",
+        medium: "h-8 px-1 py-1.5 text-sm leading-3.5",
       },
     },
 
