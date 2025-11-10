7db5271affd81282d909547fe4c3be67f61d65a7
stupilman <creworroad@naver.com>
2025-11-10 21:28:13 +0900

    refactor(checkbox): 체크박스가 block 이던 것을 inline-flex로 수정

--- src/components/CheckBox/variants/wrapperVariants.ts
+++ src/components/CheckBox/variants/wrapperVariants.ts
@@ -1,20 +1,23 @@
 import { cva, type VariantProps } from "class-variance-authority";
 
-export const checkBoxWrapper = cva(["flex items-center", "select-none"], {
-  variants: {
-    hasLabel: {
-      true: "gap-3",
-      false: "",
+export const checkBoxWrapper = cva(
+  ["inline-flex items-center", "group", "align-middle", "select-none"],
+  {
+    variants: {
+      hasLabel: {
+        true: "gap-3",
+        false: "",
+      },
+      isDisabled: {
+        true: "disabled-base pointer-events-none",
+        false: "",
+      },
     },
-    isDisabled: {
-      true: "disabled-base pointer-events-none",
-      false: "",
+    defaultVariants: {
+      hasLabel: true,
+      isDisabled: false,
     },
-  },
-  defaultVariants: {
-    hasLabel: true,
-    isDisabled: false,
-  },
-});
+  }
+);
 
 export type WrapperVariantProps = VariantProps<typeof checkBoxWrapper>;
