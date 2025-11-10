3252acdcb70de07c1264ed4a4b8b30dec1ef6f73
stupilman <creworroad@naver.com>
2025-11-10 21:28:13 +0900

    refactor(checkbox): 체크박스가 block 이던 것을 inline-flex로 수정

--- src/components/CheckBox/CheckBoxWrapper.tsx
+++ src/components/CheckBox/CheckBoxWrapper.tsx
@@ -31,7 +31,6 @@ export default function CheckBoxWrapper({
     <label
       htmlFor={id}
       className={twMerge(
-        "group",
         checkBoxWrapper({
           hasLabel: Boolean(label),
           isDisabled: disabled,
