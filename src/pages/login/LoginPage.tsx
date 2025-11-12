426000973431a8087d166f4f5d4bb6abdd1507ba
stupilman <creworroad@naver.com>
2025-11-12 10:42:44 +0900

    refactor(login): 모바일 퍼스트 로그인 페이지 레이아웃 수정 및 회원가입 버튼 추가

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -2,7 +2,7 @@ import LoginComponent from "./LoginComponent";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center px-4">
+    <div className="flex h-screen w-full items-center justify-center">
       <LoginComponent />
     </div>
   );
