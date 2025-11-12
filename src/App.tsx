b8b5cbd50624f1f5e788acd77d21d4574ccfa8a0
Sage <prestoferoce@gmail.com>
2025-11-13 03:41:17 +0900

    feat(router): react router 설치 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Routes, Route, Navigate } from "react-router";
+
+import MockLoginPage from "@/pages/MockLoginPage";
+import MockMainPage from "@/pages/MockMainPage";
+
 export default function App() {
-  return <h1>App Component</h1>;
+  return (
+    <Routes>
+      <Route path="/login" element={<MockLoginPage />} />
+      {/* TODO: Auth 유저만 / 진입할 수 있도록 Auth Gate 구현 */}
+      <Route path="/" element={<MockMainPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
