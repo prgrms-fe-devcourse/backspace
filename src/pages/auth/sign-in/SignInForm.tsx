4408c82df160c446be64e15378bb59d57830901d
stupilman <creworroad@naver.com>
2025-11-12 17:36:38 +0900

    refactor(signIn): 로그인 페이지 코드 구조 변경  (추후 로그인 기능 구현 예정)

--- src/pages/auth/sign-in/SignInForm.tsx
+++ src/pages/auth/sign-in/SignInForm.tsx
@@ -1,20 +1,41 @@
+import Button from "@/components/Button/Button";
 import Input from "@/components/Input/Input";
 
+import SocialLoginButtons from "./SocialLoginButtons";
+
 export default function SignInForm() {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // handle form submission here
+  };
+
   return (
-    <div className="flex w-full flex-col gap-5">
-      <div className="flex flex-col gap-1">
+    <form onSubmit={handleSubmit} className="flex w-full flex-col items-center gap-6">
+      <div className="flex w-full flex-col gap-1">
         <label htmlFor="email" className="select-none">
           Email:
         </label>
+
         <Input id="email" />
       </div>
-      <div className="flex flex-col gap-1">
+
+      <div className="flex w-full flex-col gap-1">
         <label htmlFor="password" className="select-none">
           Password:
         </label>
+
         <Input id="password" type="password" />
       </div>
-    </div>
+
+      <SocialLoginButtons />
+
+      <Button composition="textOnly" type="submit" size="lg" className="w-60">
+        Login
+      </Button>
+
+      <button type="button" className="cursor-pointer text-xs underline opacity-40">
+        Don’t have an account?
+      </button>
+    </form>
   );
 }
