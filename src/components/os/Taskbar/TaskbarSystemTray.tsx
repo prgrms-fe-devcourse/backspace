7689273caec98c43b0bcf96a4f43e5c60d307b84
rumpumpum <kjh04215@gmail.com>
2025-11-13 23:26:48 +0900

    feat(taskbar): day.js를 이용한 현재시간 구현

--- src/components/os/Taskbar/TaskbarSystemTray.tsx
+++ src/components/os/Taskbar/TaskbarSystemTray.tsx
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 import { taskbarSystemTray } from "./variants";
@@ -7,7 +9,7 @@ type TaskbarSystemTrayProps = React.ComponentPropsWithoutRef<"div">;
 /**
  * Taskbar 시스템 트레이 컴포넌트
  *
- * 내부에 스피커 아이콘과 시간 표시 영역이 포함되어 있습니다.
+ * 내부에 시간 표시 영역이 포함되어 있습니다.
  *
  * @component
  * @param {string} [className] - 추가 Tailwind 클래스
@@ -19,11 +21,25 @@ type TaskbarSystemTrayProps = React.ComponentPropsWithoutRef<"div">;
  * ```
  */
 export default function TaskbarSystemTray({ className, ...rest }: TaskbarSystemTrayProps) {
+  const [currentTime, setCurrentTime] = useState(dayjs());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(dayjs());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const formattedTime = currentTime.format("HH:mm");
+  const isoTime = currentTime.format("YYYY-MM-DDTHH:mm:ssZ");
+
   return (
     <div className={twMerge(taskbarSystemTray, className)} {...rest}>
-      <time className="whitespace-nowrap" dateTime="">
-        {/* 시간 표시 영역 */}
-        12:00
+      <time className="whitespace-nowrap" dateTime={isoTime}>
+        {formattedTime}
       </time>
     </div>
   );
