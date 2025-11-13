b252e722fc34766f81e489345345274334703f89
Sage <prestoferoce@gmail.com>
2025-11-13 15:59:08 +0900

    feat(type): window-app 타입에 메모 추가

--- src/types/window-app.type.ts
+++ src/types/window-app.type.ts
@@ -2,6 +2,7 @@ import Friends from "@/assets/icons/friends.svg?react";
 import Gallery from "@/assets/icons/gallery.svg?react";
 import Guestbook from "@/assets/icons/guestbook.svg?react";
 import Home from "@/assets/icons/home.svg?react";
+import Memo from "@/assets/icons/memo.svg?react";
 import Settings from "@/assets/icons/settings.svg?react";
 
 export const WINDOW_APP = {
@@ -12,6 +13,10 @@ export const WINDOW_APP = {
   GALLERY: {
     caption: "Gallery",
     icon: Gallery,
+  },
+  MEMO: {
+    caption: "Memo",
+    icon: Memo,
   },
   GUESTBOOK: {
     caption: "Guest Book",
