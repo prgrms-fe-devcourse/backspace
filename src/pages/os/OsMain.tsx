47d2f4fd4d20fe209dbef787e7941b03d8f6d796
rumpumpum <kjh04215@gmail.com>
2025-11-14 10:39:22 +0900

    refactor(os): 숏컷(아이콘)영역 ul, li 시멘틱 구조로 변경

--- src/pages/os/OsMain.tsx
+++ src/pages/os/OsMain.tsx
@@ -25,7 +25,7 @@ export default function OsMain() {
     });
   };
 
-  const handleShortcutContainerClick = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleShortcutContainerClick = (e: React.MouseEvent<HTMLUListElement>) => {
     // Shortcut 컨테이너 안에서 클릭한 경우
     const target = e.target as HTMLElement;
 
@@ -53,85 +53,101 @@ export default function OsMain() {
       {/* 바탕화면 영역 */}
       <div className="flex-1 p-4">
         {/* 숏컷 영역 */}
-        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-        <div className="flex w-16 flex-col gap-4" onClick={handleShortcutContainerClick}>
-          <Shortcut
-            Icon={WINDOW_APP.HOME.icon}
-            caption={WINDOW_APP.HOME.caption}
-            isSelected={selectedShortcutCategory === "home"}
-            isFocused={focusedShortcutCategory === "home"}
-            onClick={() => handleShortcutClick("home")}
-            onDoubleClick={() =>
-              handleShortcutDoubleClick("home", WINDOW_APP.HOME.caption, <WINDOW_APP.HOME.icon />)
-            }
-          />
-          <Shortcut
-            Icon={WINDOW_APP.GALLERY.icon}
-            caption={WINDOW_APP.GALLERY.caption}
-            isSelected={selectedShortcutCategory === "gallery"}
-            isFocused={focusedShortcutCategory === "gallery"}
-            onClick={() => handleShortcutClick("gallery")}
-            onDoubleClick={() =>
-              handleShortcutDoubleClick(
-                "gallery",
-                WINDOW_APP.GALLERY.caption,
-                <WINDOW_APP.GALLERY.icon />
-              )
-            }
-          />
-          <Shortcut
-            Icon={WINDOW_APP.MEMO.icon}
-            caption={WINDOW_APP.MEMO.caption}
-            isSelected={selectedShortcutCategory === "memo"}
-            isFocused={focusedShortcutCategory === "memo"}
-            onClick={() => handleShortcutClick("memo")}
-            onDoubleClick={() =>
-              handleShortcutDoubleClick("memo", WINDOW_APP.MEMO.caption, <WINDOW_APP.MEMO.icon />)
-            }
-          />
-          <Shortcut
-            Icon={WINDOW_APP.GUESTBOOK.icon}
-            caption={WINDOW_APP.GUESTBOOK.caption}
-            isSelected={selectedShortcutCategory === "guestbook"}
-            isFocused={focusedShortcutCategory === "guestbook"}
-            onClick={() => handleShortcutClick("guestbook")}
-            onDoubleClick={() =>
-              handleShortcutDoubleClick(
-                "guestbook",
-                WINDOW_APP.GUESTBOOK.caption,
-                <WINDOW_APP.GUESTBOOK.icon />
-              )
-            }
-          />
-          <Shortcut
-            Icon={WINDOW_APP.FRIENDS.icon}
-            caption={WINDOW_APP.FRIENDS.caption}
-            isSelected={selectedShortcutCategory === "friends"}
-            isFocused={focusedShortcutCategory === "friends"}
-            onClick={() => handleShortcutClick("friends")}
-            onDoubleClick={() =>
-              handleShortcutDoubleClick(
-                "friends",
-                WINDOW_APP.FRIENDS.caption,
-                <WINDOW_APP.FRIENDS.icon />
-              )
-            }
-          />
-          <Shortcut
-            Icon={WINDOW_APP.SETTINGS.icon}
-            caption={WINDOW_APP.SETTINGS.caption}
-            isSelected={selectedShortcutCategory === "settings"}
-            isFocused={focusedShortcutCategory === "settings"}
-            onClick={() => handleShortcutClick("settings")}
-            onDoubleClick={() =>
-              handleShortcutDoubleClick(
-                "settings",
-                WINDOW_APP.SETTINGS.caption,
-                <WINDOW_APP.SETTINGS.icon />
-              )
-            }
-          />
-        </div>
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
+        <ul
+          className="flex w-16 flex-col gap-4"
+          onClick={handleShortcutContainerClick}
+          aria-label="바탕화면 바로가기"
+        >
+          <li>
+            <Shortcut
+              Icon={WINDOW_APP.HOME.icon}
+              caption={WINDOW_APP.HOME.caption}
+              isSelected={selectedShortcutCategory === "home"}
+              isFocused={focusedShortcutCategory === "home"}
+              onClick={() => handleShortcutClick("home")}
+              onDoubleClick={() =>
+                handleShortcutDoubleClick("home", WINDOW_APP.HOME.caption, <WINDOW_APP.HOME.icon />)
+              }
+            />
+          </li>
+          <li>
+            <Shortcut
+              Icon={WINDOW_APP.GALLERY.icon}
+              caption={WINDOW_APP.GALLERY.caption}
+              isSelected={selectedShortcutCategory === "gallery"}
+              isFocused={focusedShortcutCategory === "gallery"}
+              onClick={() => handleShortcutClick("gallery")}
+              onDoubleClick={() =>
+                handleShortcutDoubleClick(
+                  "gallery",
+                  WINDOW_APP.GALLERY.caption,
+                  <WINDOW_APP.GALLERY.icon />
+                )
+              }
+            />
+          </li>
+          <li>
+            <Shortcut
+              Icon={WINDOW_APP.MEMO.icon}
+              caption={WINDOW_APP.MEMO.caption}
+              isSelected={selectedShortcutCategory === "memo"}
+              isFocused={focusedShortcutCategory === "memo"}
+              onClick={() => handleShortcutClick("memo")}
+              onDoubleClick={() =>
+                handleShortcutDoubleClick("memo", WINDOW_APP.MEMO.caption, <WINDOW_APP.MEMO.icon />)
+              }
+            />
+          </li>
+          <li>
+            <Shortcut
+              Icon={WINDOW_APP.GUESTBOOK.icon}
+              caption={WINDOW_APP.GUESTBOOK.caption}
+              isSelected={selectedShortcutCategory === "guestbook"}
+              isFocused={focusedShortcutCategory === "guestbook"}
+              onClick={() => handleShortcutClick("guestbook")}
+              onDoubleClick={() =>
+                handleShortcutDoubleClick(
+                  "guestbook",
+                  WINDOW_APP.GUESTBOOK.caption,
+                  <WINDOW_APP.GUESTBOOK.icon />
+                )
+              }
+            />
+          </li>
+          <li>
+            <Shortcut
+              Icon={WINDOW_APP.FRIENDS.icon}
+              caption={WINDOW_APP.FRIENDS.caption}
+              isSelected={selectedShortcutCategory === "friends"}
+              isFocused={focusedShortcutCategory === "friends"}
+              onClick={() => handleShortcutClick("friends")}
+              onDoubleClick={() =>
+                handleShortcutDoubleClick(
+                  "friends",
+                  WINDOW_APP.FRIENDS.caption,
+                  <WINDOW_APP.FRIENDS.icon />
+                )
+              }
+            />
+          </li>
+          <li>
+            <Shortcut
+              Icon={WINDOW_APP.SETTINGS.icon}
+              caption={WINDOW_APP.SETTINGS.caption}
+              isSelected={selectedShortcutCategory === "settings"}
+              isFocused={focusedShortcutCategory === "settings"}
+              onClick={() => handleShortcutClick("settings")}
+              onDoubleClick={() =>
+                handleShortcutDoubleClick(
+                  "settings",
+                  WINDOW_APP.SETTINGS.caption,
+                  <WINDOW_APP.SETTINGS.icon />
+                )
+              }
+            />
+          </li>
+        </ul>
       </div>
       {/* 태스크바 영역 */}
       <Taskbar />
