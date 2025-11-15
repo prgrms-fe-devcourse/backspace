12079b8c22b775838b41191254854103be4e38f1
Sage <prestoferoce@gmail.com>
2025-11-16 05:29:27 +0900

    refactor(signin): 로그인 페이지 리팩토링

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router";
+import { twMerge } from "tailwind-merge";
+
+export default function AuthLayout() {
+  return (
+    <main
+      className={twMerge(
+        "relative inset-0 h-dvh max-h-dvh w-dvw max-w-dvw",
+        "flex items-center justify-center overflow-hidden",
+        "from-primary to-secondary bg-linear-to-br"
+      )}
+    >
+      <section
+        className={twMerge(
+          "bevel-default absolute inset-0 inline-flex h-full w-full flex-col p-[3px] focus:outline-none",
+          "md:h-150 md:w-110",
+          "md:inset-auto md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2"
+        )}
+      >
+        <Outlet />
+      </section>
+    </main>
+  );
+}
--- src/pages/auth/sign-in/SignInPage.tsx
+++ src/pages/auth/sign-in/SignInPage.tsx
@@ -1,5 +1,36 @@
-import SignInComponent from "./SignInComponent";
+import { Link } from "react-router";
+
+import Logo from "@/assets/logo/logo.svg?react";
+import TitleBar from "@/components/window/TitleBar/TitleBar";
+
+import SignInForm from "./SignInForm";
+import SocialLoginButtons from "./SocialLoginButtons";
 
 export default function SignInPage() {
-  return <SignInComponent />;
+  return (
+    <>
+      <TitleBar icon={<Logo />} text="Welcome to BackSpace" />
+
+      <div className="lex-1 flex h-full w-full flex-col items-center justify-center gap-5 p-4">
+        <div className="flex w-full flex-col items-center justify-center gap-4">
+          <Logo />
+          <p className="text-center select-none">Enter your email and password to log on.</p>
+        </div>
+
+        <SignInForm />
+
+        <div className="flex w-full items-center gap-4">
+          <div className="border-text-default/30 flex-1 border-t" />
+          <span className="text-text-default/70 text-xs select-none">or</span>
+          <div className="border-text-default/30 flex-1 border-t" />
+        </div>
+
+        <SocialLoginButtons />
+
+        <Link to="/signup" className="text-xs underline opacity-60">
+          Don’t have an account?
+        </Link>
+      </div>
+    </>
+  );
 }
