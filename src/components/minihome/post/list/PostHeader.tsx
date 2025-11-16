7def2614f7a046a00d15fe3500c93412e81f3531
stupilman <creworroad@naver.com>
2025-11-16 17:45:46 +0900

    feat(post): 게시판 UI 및 dev 충돌 해결

--- src/components/minihome/post/list/PostHeader.tsx
+++ src/components/minihome/post/list/PostHeader.tsx
@@ -1,3 +1,16 @@
-export default function PostHeader() {
-  return <h1>PostHeader Component</h1>;
+import { SquarePen } from "lucide-react";
+
+import Button from "@/components/common/Button/Button";
+
+export default function PostHeader({ onClick }: { onClick: () => void }) {
+  return (
+    <div className="flex justify-between">
+      {/* TODO: 나중에 전체 게시글 옆에 length로 몇개인지 불러오기 */}
+      <p>전체 게시글</p>
+      <Button onClick={onClick}>
+        <SquarePen width={12} />
+        글쓰기
+      </Button>
+    </div>
+  );
 }
--- src/components/minihome/post/write/PostWrite.tsx
+++ src/components/minihome/post/write/PostWrite.tsx
@@ -1,3 +1,43 @@
-export default function PostWrite() {
-  return <h1>PostWrite Component</h1>;
+import { Send, X } from "lucide-react";
+import { useState } from "react";
+
+import Button from "@/components/common/Button/Button";
+import Input from "@/components/common/Input/Input";
+import TextArea from "@/components/common/TextArea/TextArea";
+
+export default function PostWrite({ onClose }: { onClose: () => void }) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <form className="flex h-full flex-col gap-3.5 p-3.5">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="title">제목</label>
+        <Input
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="제목을 입력하세요"
+        />
+      </div>
+      <div className="flex flex-1 flex-col gap-2">
+        <label htmlFor="content">내용</label>
+        <TextArea
+          id="content"
+          value={content}
+          className="flex-1"
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="내용을 입력하세요"
+        />
+      </div>
+      <div className="flex w-full justify-end gap-2">
+        <Button size="lg" onClick={onClose}>
+          <X size={14} /> 취소
+        </Button>
+        <Button size="lg">
+          <Send size={14} /> 등록
+        </Button>
+      </div>
+    </form>
+  );
 }
