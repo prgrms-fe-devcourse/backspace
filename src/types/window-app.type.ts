cd814d291f0b73db1c4d6f83637c9a1645a44324
rumpumpum <kjh04215@gmail.com>
2025-11-16 21:57:25 +0900

    refactor(os): 임시 컴포넌트 todoComponent 개선

--- src/types/window-app.type.ts
+++ src/types/window-app.type.ts
@@ -1,4 +1,4 @@
-import { createElement, type ComponentType } from "react";
+import { createElement } from "react";
 
 import Friends from "@/assets/icons/friends.svg?react";
 import Gallery from "@/assets/icons/gallery.svg?react";
@@ -8,7 +8,7 @@ import Memo from "@/assets/icons/memo.svg?react";
 import Settings from "@/assets/icons/settings.svg?react";
 import MiniHome from "@/pages/minihome/MiniHome";
 
-const todoComponent = (): ComponentType => () => createElement("div", null, "(구현 예정)");
+const todoComponent = () => createElement("div", null, "(구현 예정)");
 
 export const WINDOW_APP = {
   HOME: {
@@ -21,31 +21,31 @@ export const WINDOW_APP = {
     category: "gallery",
     caption: "Gallery",
     icon: Gallery,
-    component: todoComponent(),
+    component: todoComponent,
   },
   MEMO: {
     category: "memo",
     caption: "Memo",
     icon: Memo,
-    component: todoComponent(),
+    component: todoComponent,
   },
   GUESTBOOK: {
     category: "guestbook",
     caption: "Guest Book",
     icon: Guestbook,
-    component: todoComponent(),
+    component: todoComponent,
   },
   FRIENDS: {
     category: "friends",
     caption: "Friends",
     icon: Friends,
-    component: todoComponent(),
+    component: todoComponent,
   },
   SETTINGS: {
     category: "settings",
     caption: "Settings",
     icon: Settings,
-    component: todoComponent(),
+    component: todoComponent,
   },
 } as const;
 
