60210d9a54237ee5a822b1ad506d157ad3779438
suyeon <suyeon1104@gmail.com>
2025-11-17 21:01:53 +0900

    style(home-page): 여백 크기 통일

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import OsMain from "@/pages/os/OsMain";
 import ProtectedRoute from "@/router/ProtectedRoute";
 import PublicRoute from "@/router/PublicRoute";
 
-import HomePage from "./components/Page/HomePage";
-
 export default function App() {
   return (
     <>
@@ -23,7 +21,6 @@ export default function App() {
         </Route>
         <Route element={<ProtectedRoute />}>
           <Route path="/" element={<OsMain />} />
-          <Route path="/homepage/:homepageId" element={<HomePage />} />
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
