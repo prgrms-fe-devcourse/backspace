357da4a10e40c87c7a498e301617e02798bf0120
rumpumpum <kjh04215@gmail.com>
2025-11-20 07:38:21 +0900

    refactor(home): 브레이크 포인트로 전체화면 시 좌측 프로필 영역에 py 패딩 추가

--- src/features/minihome/MiniHome.tsx
+++ src/features/minihome/MiniHome.tsx
@@ -51,7 +51,7 @@ export default function MiniHome({ windowId, ownerId, tab = MINIHOME_TABS.home }
           </Tabs.Trigger>
         ))}
       </Tabs.List>
-      <Tabs.Content value={MINIHOME_TABS.home}>
+      <Tabs.Content value={MINIHOME_TABS.home} className="min-h-0 flex-1">
         <Activity>
           <HomePage ownerId={ownerId} setActiveTab={setActiveTab} />
         </Activity>
