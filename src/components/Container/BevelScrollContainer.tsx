815ecb7f265b4d0ce40a913bf3c569ed1778f86a
rumpumpum <kjh04215@gmail.com>
2025-11-19 23:23:53 +0900

    refactor(container): props로 className을 받게 수정

--- src/components/Container/BevelScrollContainer.tsx
+++ src/components/Container/BevelScrollContainer.tsx
@@ -1,9 +1,17 @@
 import type { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
-export default function BevelScrollContainer({ children }: PropsWithChildren) {
+export default function BevelScrollContainer({
+  children,
+  className,
+}: PropsWithChildren<{ className?: string }>) {
   return (
     <div className="bevel-pressed h-full w-full overflow-hidden p-1">
-      <div className="scrollbar bg-text-invert h-full w-full overflow-y-auto p-4">{children}</div>
+      <div
+        className={twMerge("scrollbar bg-text-invert h-full w-full overflow-y-auto p-4", className)}
+      >
+        {children}
+      </div>
     </div>
   );
 }
